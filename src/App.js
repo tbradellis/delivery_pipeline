a876c80f6febfb3cceea7e7a7488b1d464d84524
Brad Ellis <bradellis10@gmail.com>
2018-12-27 22:56:51 -0800

    docker files

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+              Hello there!
+              Zippity do dah!
           </p>
           <a
             className="App-link"
