559511dc9fd24fb2364bdb8e387543fac89617f6
Brad Ellis <bradellis10@gmail.com>
2018-12-28 13:12:02 -0800

    changed welcome screen

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-              Hello there!
+              Welcome to my page!
               Zippity do dah!
           </p>
           <a
